2b11f6f1f790171a75e53df329aba1d083aaecc8
Melvin Kisten <iammelvink@gmail.com>
2021-06-10 09:58:14 +0200

    included authors in code

--- src/Encryption/EncryptionMain.java
+++ src/Encryption/EncryptionMain.java
@@ -1,12 +1,14 @@
 package Encryption;
 
+/**
+ *
+ * @author Melvin Kisten and Smous
+ */
 public class EncryptionMain {
 
     public static void main(String[] args) {
 
         EncryptionProgram encryt = new EncryptionProgram();
-
-        // encryt.setKey();
     }
 
 }
--- src/Encryption/EncryptionProgram.java
+++ src/Encryption/EncryptionProgram.java
@@ -1,7 +1,11 @@
-
 package Encryption;
 
 import java.util.*;
+
+/**
+ *
+ * @author Melvin Kisten and Smous
+ */
 public class EncryptionProgram {
 
     private Scanner inp;
@@ -11,61 +15,53 @@ public class EncryptionProgram {
     private char charts;
     private char[] letters;
 
+    // Constractor
+    EncryptionProgram() {
 
-    //Constractor
-     EncryptionProgram() {
-
-         inp = new Scanner(System.in);
-         ram = new Random();
-         list = new ArrayList();
-         newList = new ArrayList();
-         charts = ' ' ;
-         setKey();
-         callMethods();
+        inp = new Scanner(System.in);
+        ram = new Random();
+        list = new ArrayList();
+        newList = new ArrayList();
+        charts = ' ';
+        setKey();
+        callMethods();
     }
 
+    // Choose the method you want to call
+    private void callMethods() {
 
-            // Choose the method you want to call
-          private void callMethods(){
+        while (true) {
+            System.out.println("----------------------------------------------------------------");
+            System.out.println("Type \"e\" to Encrypt\n\"d\" to Decrypt\n\"q\" to Quit");
 
-                while(true) {
-                    System.out.println("----------------------------------------------------------------");
-                System.out.println("Type \"e\" to Encrypt\n\"d\" to Decrypt\n\"q\" to Quit");
+            char response = Character.toUpperCase/* changing the word to uppercase */(
+                    inp.nextLine().charAt(0)/* Getting the first letter */);
 
-
-                char response = Character.toUpperCase/*changing the word to uppercase*/(inp.nextLine().charAt(0)/*Getting the first letter*/);
-
-
-                switch (response){
-                    //Calling methods using the first Letter
+            switch (response) {
+                // Calling methods using the first Letter
                 case 'E':
 
-                  encrypte();
+                    encrypte();
                     break;
-                     case 'D':
-                    //call a method
-                         decrypte();
+                case 'D':
+                    // call a method
+                    decrypte();
                     break;
-                     case 'Q':
+                case 'Q':
                     quit();
-                         System.out.println("Thank you");
+                    System.out.println("Thank you");
                     break;
                 default:
                     System.out.println("please choose from the list");
-                }
-                System.out.println();
+            }
+            System.out.println();
 
-                }
+        }
 
+    }
 
-
-          }
-
-
-
-
-    public void setKey(){
-        charts = ' ' ;
+    public void setKey() {
+        charts = ' ';
         list.clear();
         newList.clear();
 
@@ -78,74 +74,73 @@ public class EncryptionProgram {
         Collections.shuffle(newList);
         System.out.println("created");
 
+    }
+
+    // private void getKey(){
+    // System.out.println("key----");
+    //
+    // for(Character x: list){
+    // System.out.print(x);
+    // }
+    //
+    // for(Character x : newList){
+    // System.out.print(x);
+    // }
+    // System.out.println("newKey");
+    //
+    // }
+
+    // encryption
+    private void encrypte() {
+        System.out.println("-----enter the password you want to Encrypt----");
+        String password = inp.nextLine();
+        letters = password.toCharArray();
+
+        for (int i = 0; i < letters.length; i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (letters[i] == list.get(j)) {
+                    letters[i] = newList.get(j);
+                    break;
+                }
+
+            }
+
+        }
+        System.out.println("encrypted password");
+        for (char x : letters) {
+            System.out.print(x);
+        }
+        System.out.println();
 
     }
 
-//       private void getKey(){
-//          System.out.println("key----");
-//
-//          for(Character x: list){
-//              System.out.print(x);
-//          }
-//
-//          for(Character x : newList){
-//              System.out.print(x);
-//          }
-//          System.out.println("newKey");
-//
-//    }
+    // decryption
+    private void decrypte() {
 
-       //encryption
-      private void encrypte(){
-            System.out.println("-----enter the password you want to Encrypt----");
-            String password = inp.nextLine();
-            //smous
-            letters = password.toCharArray();
+        System.out.println("enter an encrypted password to decrypt it ");
+        String password = inp.nextLine();
+        letters = password.toCharArray();
 
-            for (int i = 0; i < letters.length; i++) {
-                for (int j = 0; j < list.size(); j++) {
-                    if(letters[i]==list.get(j)){
-                        letters[i] = newList.get(j);
-                    break;
-                    }
+        for (int i = 0; i < letters.length; i++) {
+            for (int j = 0; j < newList.size(); j++) {
+                if (letters[i] == newList.get(j)) {
+                    letters[i] = list.get(j);
 
                 }
 
             }
-            System.out.println("encrypted password");
-            for(char x : letters){
-                System.out.print(x);
-            }
-            System.out.println();
+
+        }
+        // Display the output
+        System.out.println("Decrypted password");
+        for (char x : letters) {
+            System.out.print(x);
+        }
+        System.out.println();
 
     }
 
-      //decryption
-      private void decrypte(){
-
-              System.out.println("enter an encrypted password to decrypt it ");
-              String password = inp.nextLine();
-              letters = password.toCharArray();
-
-              for (int i = 0; i < letters.length; i++) {
-                  for (int j = 0; j < newList.size(); j++) {
-                      if(letters[i]== newList.get(j)){
-                          letters[i] = list.get(j);
-
-                      }
-
-                  }
-
-              }
-              //Display the output
-              System.out.println("Decrypted password");
-              for( char x : letters){
-                  System.out.print(x);
-              }
-              System.out.println();
-
+    private void quit() {
+        System.exit(0);
     }
-      private void quit(){
-          System.exit(0);
-      }
 }
